906fd8f12f142127dcc0d8163c0423d2eda0b6db
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-02 20:26:17 +0300

    DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const body=document.querySelector('p');
+body.innerHTML= '<p>JavaScript is so cool. It lets me add text to my page programmatically.</p>'
+//add an event
+const event = document.createEvent('Event');
+event.initEvent( 'DOMContentLoaded', true, true );
+    window.document.dispatchEvent( event );
+    body.querySelector('p');
+    body.textContent = ('This is really cool!');
+    console.log(body.textContent);
